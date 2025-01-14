74a699de802e313f2b529791283ae6d1f3242eca
Bobbmyth <legeay.antoine@gmail.com>
2025-01-14 14:16:12 +0100

    ça avance !

---
+++ WebApplication1/Models/Personne.cs
@@ -0,0 +1,20 @@
+﻿namespace WebApplication1.Models
+{
+    public class Personne
+    {
+
+        public int Id { get; set; }
+        public double Prix { get; set; }
+
+        public decimal Tva { get; set;}
+
+        public string Nom { get; set;}
+
+        public bool Status { get; set;}
+
+        public float Montant { get; set;}
+
+        
+
+    }
+}
